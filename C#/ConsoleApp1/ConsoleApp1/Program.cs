d61ad956202265062a391567c59d6f7056c8f999
arnomoller <131721083+arnomoller@users.noreply.github.com>
2023-04-26 10:31:55 +0200

    Add files via upload

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Question2
+{
+
+    static public void Main()
+    {
+
+        // Creating a count variable
+        var total = 0;
+
+        // creating array of numbers
+        int[] numberlist = {56, 85, 42, 73, 65, 30, 90, 75 };
+
+        // Counting the number of elements
+        total = numberlist.Count();
+
+        // Displaying the count
+        
+        Console.Write("numlist Array:\n");
+            Console.WriteLine("Total Number of Elements: " + total);
+        Console.Write(Array.Exists(numberlist, element => element == 30 ));
+        Console.Write("\n");
+        Console.Write(Array.Exists(numberlist, element => element == 70 ));
+
+    }
+}
---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace HeapSortDemo
+{
+    public class Question4
+    {
+        static void heapSort(int[] arr, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(arr, n, i);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+                heapify(arr, i, 0);
+            }
+        }
+        static void heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+                heapify(arr, n, largest);
+            }
+        }
+        public static void Main()
+        {
+            int[] arr = {50, 33, 78, -23, 90  };
+            int n = 5, i;
+            Console.WriteLine("Heap Sort");
+            Console.Write("Initial array(hst) is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            heapSort(arr, 5);
+            Console.Write(" Sorted Array(hst) is: ");
+           for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+               
+            }
+        }
+       
+    }
+}
